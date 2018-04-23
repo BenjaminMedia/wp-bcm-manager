f405456fc57c7cb0fe51a5ef68de515b19125e32
Frederik Rabøl <frederik-rm@hotmail.com>
2018-04-23 09:34:15 +0200

    changed setting text to be easier to understand

--- src/Bonnier/BCM/Settings/SettingsPage.php
+++ src/Bonnier/BCM/Settings/SettingsPage.php
@@ -29,7 +29,7 @@ class SettingsPage {
 	private $arrFields = [
 		'enabled' => [
 			'type' => 'checkbox',
-			'name' => 'Enabled? This will still show all the BCM tags, but disable the banners themselves',
+			'name' => 'Enable banners (BCM tags are always shown)',
 		],
 		'type' => [
 			'type' => 'text',
